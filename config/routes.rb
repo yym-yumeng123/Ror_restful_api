04206d2a33a76398881df10cd32a9efea7bd8fd5
“yym-yumeng123 <18026493873@163.com>
2022-02-28 23:20:52 +0800

    创建通知控制器

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # 查看版本路由
   get 'monitors/version'
+  # 钉钉通知路由
+  post 'notices/notices'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
