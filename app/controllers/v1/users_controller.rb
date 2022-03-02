1a4ebbc44f2b9f60c4e4c538b79d191ac63fc255
“yym-yumeng123 <18026493873@163.com>
2022-03-02 22:59:26 +0800

    简单错误处理

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -16,12 +16,19 @@ class V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: v1_user_path(@user)
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    begin
+      if @user.save
+        render json: @user, status: :created, location: v1_user_path(@user)
+      else
+        # 保存失败，直接返回错误码，和固定的错误信息
+        # 响应码为202，表示请求服务器接收到了；可以返回200
+        render json: {status: '20', message: '参数错误, 请稍后再试! '}, status: :ok
+        # render json: @user.errors, status: :unprocessable_entity
+      end
+    rescue
+      render json: {status: '10', message: '未知错误, 请稍后再试!'}, status: :accepted
     end
+    
   end
 
   # PATCH/PUT /users/1
